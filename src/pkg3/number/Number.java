0ffa9c91ec80acad4bec325abc29ac5c0a552af7
daniel.knespl <daniel.knespl@W10419.sps-cl.local>
2018-03-26 14:20:38 +0200

    tři čísla 2.0 odsazený

--- src/pkg3/number/Number.java
+++ src/pkg3/number/Number.java
@@ -4,7 +4,9 @@
  * and open the template in the editor.
  */
 package pkg3.number;
+
 import java.util.Scanner;
+
 /**
  *
  * @author daniel.knespl
@@ -12,24 +14,24 @@ import java.util.Scanner;
 public class Number {
 
     public static void main(String[] args) {
-        Scanner a = new Scanner(System.in,"ASCII");
+        Scanner a = new Scanner(System.in, "ASCII");
         System.out.println("vloz cisla");
         double max = 0;
         double x = 0;
         double s = 1;
         for (int i = 0; i < 3; i++) {
-        x = a.nextDouble();
-        if (max==x){
-                s=s+1;
-            }  
-            if (max<x){
-                max=x;
+            x = a.nextDouble();
+            if (max == x) {
+                s = s + 1;
+            }
+            if (max < x) {
+                max = x;
             }
         }
         System.out.println();
-        System.out.print("nejvetsi je: "+max+", ");
-        if(1<s){
-        System.out.println(s+"-krát");
+        System.out.print("nejvetsi je: " + max + ", ");
+        if (1 < s) {
+            System.out.println(s + "-krát");
         }
     }
 }
